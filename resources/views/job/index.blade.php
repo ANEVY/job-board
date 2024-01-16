5db121d40dd53859c50f13592d37d999dbdbc442
Akombo Neville Akwo <akomboneville55@gmail.com>
2024-01-16 02:16:20 +0100

    breadcrumb navigation

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
+    <x-breadcrumb class="mb-4" :links="['Jobs'=>route('jobs.index')]"></x-breadcrumb>
     @foreach ($jobs as $job)
         <x-job-card class="mb-4" :job="$job">
             <div class="my-3">
--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -1,5 +1,4 @@
 <x-layout>
-    <x-job-card :job="$job">
-
-    </x-job-card>
+    <x-breadcrumb class="mb-4" :links="['Jobs'=>route('jobs.index'), $job->title => '#']"></x-breadcrumb>
+    <x-job-card :job="$job"></x-job-card>
 </x-layout>
