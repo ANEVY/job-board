96ee240048b72c25b80737a80056474fa9eacbb5
Akombo Neville Akwo <akomboneville55@gmail.com>
2024-01-16 02:49:58 +0100

    filter jobs by search text

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,20 +1,24 @@
 <x-layout>
     <x-breadcrumb class="mb-4" :links="['Jobs'=>route('jobs.index')]"></x-breadcrumb>
     <x-card class="mb-4 text-sm">
-        <div class="mb-4 grid grid-cols-2 gap-4">
-            <div>
-                <div class="mb-1 font-semibold">Search</div>
-                <x-text-input name="search" value="" placeholder="Search for any text" ></x-text-input>
-            </div>
-            <div>
-                <div class="mb-1 font-semibold">Salary</div>
-                <div class="flex space-x-4">
-                    <x-text-input name="min_salary" value="" placeholder="From" ></x-text-input>
-                    <x-text-input name="max_salary" value="" placeholder="To" ></x-text-input>
+        <form action="" method="get">
+            <div class="mb-4 grid grid-cols-2 gap-4">
+                <div>
+                    <div class="mb-1 font-semibold">Search</div>
+                    <x-text-input name="search" value="" placeholder="Search for any text" ></x-text-input>
                 </div>
+                <div>
+                    <div class="mb-1 font-semibold">Salary</div>
+                    <div class="flex space-x-4">
+                        <x-text-input name="min_salary" value="" placeholder="From" ></x-text-input>
+                        <x-text-input name="max_salary" value="" placeholder="To" ></x-text-input>
+                    </div>
+                </div>
+    
             </div>
+            <button type="submit" class="btn w-full">Filter</button>
+        </form>
 
-        </div>
     </x-card>
     @foreach ($jobs as $job)
         <x-job-card class="mb-4" :job="$job">
--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -1,4 +1,6 @@
 <x-layout>
     <x-breadcrumb class="mb-4" :links="['Jobs'=>route('jobs.index'), $job->title => '#']"></x-breadcrumb>
-    <x-job-card :job="$job"></x-job-card>
+    <x-job-card :job="$job">
+        <p class="text-sm text-slate-500">{!!nl2br( e( $job->description))!!}</p>
+    </x-job-card>
 </x-layout>
