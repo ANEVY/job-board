084954f2727634ab6563da246b2e9c132bbe63c8
Akombo Neville Akwo <akomboneville55@gmail.com>
2024-01-20 18:31:13 +0100

    cancelling job application

--- resources/views/my_job_application/index.blade.php
+++ resources/views/my_job_application/index.blade.php
@@ -1,23 +1,34 @@
 <x-layout>
     <x-breadcrumb :links="['My Job Applications' => '#']"></x-breadcrumb>
     <div class="my-3">
-        @foreach ($applications as $application )
-        <x-job-card :job="$application->job">
-            <div class="flex justify-between text-xs align-middle">
-                <div>
+        @forelse ($applications as $application)
+            <x-job-card :job="$application->job">
+                <div class="flex justify-between text-xs align-middle">
                     <div>
-                        <div>Applied {{$application->created_at->diffForHumans()}}</div>
-                        <div>Other {{Str::plural('applicant',$application->job->job_applications_count-1)}} {{$application->job->job_applications_count-1}}</div>
-                        <div>Your asking salary {{$application->salary}}</div>
-                        <div>Average asking salary {{ number_format($application->job->job_applications_avg_salary)}}</div>
+                        <div>
+                            <div>Applied {{$application->created_at->diffForHumans()}}</div>
+                            <div>Other {{Str::plural('applicant',$application->job->job_applications_count-1)}} {{$application->job->job_applications_count-1}}</div>
+                            <div>Your asking salary {{$application->salary}}</div>
+                            <div>Average asking salary {{ number_format($application->job->job_applications_avg_salary)}}</div>
+                        </div>
+                        
+                    </div>
+                    <div>
+                        <form action="{{route('my-job-application.destroy',$application)}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <x-button>Cancel</x-button>
+                        </form>
                     </div>
                     
                 </div>
-                
-            </div>
-        </x-job-card>
-    
-        @endforeach
+            </x-job-card>
+        @empty
+        <div>
+            <p class="mb-2">No job application yet</p>
+            <x-link-button :href="route('jobs.index')">Apply</x-link-button>
+        </div>
+        @endforelse
     </div>
 
 </x-layout>
