ca32647563e7388a00e70b70da4bc2f725e24007
Akombo Neville Akwo <akomboneville55@gmail.com>
2024-01-20 21:45:57 +0100

    show error in radio group component

--- resources/views/components/radio-group.blade.php
+++ resources/views/components/radio-group.blade.php
@@ -9,5 +9,10 @@
             <span>{{$option}}</span>
         </x-label>       
     @endforeach
+    @error($name)
+    <div class="mt-1 text-xs text-red-500">
+      {{ $message }}
+    </div>
+  @enderror
 
 </div>
